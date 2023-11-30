eae0b85c9e49473f7fb3395ad5e27aca52e8ea2d
Genesis Patiño <62777013+genepatino@users.noreply.github.com>
2023-11-29 20:23:38 -0500

    Layout WeatherResults

---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,12 @@
+import { WeatherResults } from "../WeatherResults";
+import { Wrapper } from "./styled";
+
+function Layout() {
+  return (
+    <Wrapper>
+      <WeatherResults />
+    </Wrapper>
+  );
+}
+
+export { Layout };
